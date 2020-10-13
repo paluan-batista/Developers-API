b5efc720670b37b85c4403821555b57266ac323c
paluan batista <paluan.batista@idtrust.com.br>
2020-10-13 10:46:56 -0300

    adicionando arquivos iniciais ao projeto

---
+++ src/main/java/com/developers/developersApi/DevelopersApiApplication.java
@@ -0,0 +1,13 @@
+package com.developers.developersApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developers/developersApi/DevelopersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developers.developersApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
