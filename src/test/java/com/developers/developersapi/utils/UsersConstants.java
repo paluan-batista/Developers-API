101a3535cf07011b1fb7c26e81ddfcd1162114b2
paluan batista <paluan.batista@idtrust.com.br>
2020-10-20 12:04:38 -0300

    adicionando classe para manipulação de constantes

---
+++ src/test/java/com/developers/developersapi/utils/UsersConstants.java
@@ -0,0 +1,8 @@
+package com.developers.developersapi.utils;
+
+public interface UsersConstants {
+
+	static final String FULL_NAME = "Paluan Batista";
+	static final String IDENTIFIER = "6666666";
+
+}
