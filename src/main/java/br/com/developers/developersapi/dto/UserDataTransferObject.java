62e59f8ea6a9ac8c022815e99b22eb63eeac97d7
paluan batista <paluan.batista@idtrust.com.br>
2020-10-22 10:17:21 -0300

    Alterando importação

--- src/main/java/br/com/developers/developersapi/dto/UserDataTransferObject.java
+++ src/main/java/br/com/developers/developersapi/dto/UserDataTransferObject.java
@@ -1,8 +1,7 @@
 package br.com.developers.developersapi.dto;
 
+import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sun.istack.NotNull;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
